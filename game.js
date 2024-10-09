24eb67cdfc0a23afe444df5756b054523381203e
Jack Setaro <23753068+JackSetaro@users.noreply.github.com>
2024-10-09 01:19:03 +0000

    Add GetComputerChoice
    Add logic for computing the computers guess

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let ranNum = Math.random();
+
+    if (ranNum < 0.34){
+        return "rock";
+    } else if (ranNum >= 0.34 && ranNum < 0.67){
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
