0f325e6a8a8f417359638d634c326d9800ed3f70
Jack Setaro <23753068+JackSetaro@users.noreply.github.com>
2024-10-09 01:33:27 +0000

    Add GetHumanChoice
    Add function for getting the humans choice

--- game.js
+++ game.js
@@ -10,4 +10,12 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    let choice = prompt("Enter your choice")
+    while (choice != "rock" && choice != "paper" && choice != "scissors"){
+        choice = prompt("Enter your choice")
+    }
+    return choice;
+}
+
 console.log(getComputerChoice())
